37f06d4cde32063519994ff11729717aaebe12d6
vadimpetryuk <109548324+vadimpetryuk@users.noreply.github.com>
2022-08-15 21:34:32 -0600

    Add files via upload

---
+++ src/main/java/SERVICES/RequestsService.java
@@ -0,0 +1,41 @@
+package SERVICES;
+import DAO.RequestsDAO;
+import POJO.Requests;
+import java.util.List;
+public class RequestsService {
+    private RequestsDAO dao;
+
+    public RequestsService() {
+        this.dao = new RequestsDAO();
+    }
+
+    public void saveRequests(Requests requests) {
+        dao.create(requests);
+    }
+
+    public Requests getRequests(int id) {
+        return dao.read(id);
+    }
+
+    public List<Requests> getAllRequests() {
+        return dao.readAll();
+    }
+    public List<Requests> getRequestsForUser(int userId) {
+        List<Requests> requestList = dao.readAll();
+
+        for(Requests requests : requestList) {
+            if(requests.getUser_id() != (userId)) {
+                requestList.remove(requests);
+            }
+        }
+
+        return requestList;
+    }
+    public void updateRequests(Requests requests) {
+        dao.update(requests);
+    }
+
+    public void deleteRequests(int id) {
+        dao.delete(id);
+    }
+}
